1232ec80b7bda9329e56ec1c989e8df63b44852e
Manchini1337 <mezureno@gmail.com>
2022-11-08 22:37:23 +0100

    updating axios config

--- src/utils/api/axios.interceptor.js
+++ src/utils/api/axios.interceptor.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://manchini1337bookingapi.herokuapp.com/api/',
   withCredentials: true,
 });
